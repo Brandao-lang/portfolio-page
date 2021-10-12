41c6a6da1d7179424ae1c56d37d68b9bf2205bcb
Brandao-lang <elijahbrandao@gmail.com>
2021-10-12 14:31:24 -0400

    server updates

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 7000
+
+app.use(express.static(path.join(__dirname, 'Client/build')))
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/Client/build/index.html'));
+  });
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
