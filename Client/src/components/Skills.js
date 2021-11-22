dd06ea8c2e6f1c40edace8ee81ca7b45be0689a0
Brandao-lang <elijahbrandao@gmail.com>
2021-11-22 12:52:41 -0500

    icons updated

--- Client/src/components/Skills.js
+++ Client/src/components/Skills.js
@@ -11,7 +11,8 @@ export default function Skills () {
                 Skills
             </h2>
                 <div className='proficient-icons' style={{display: 'flex', flexWrap:'wrap', gap:'30px'}}>
-                    <svg data-aos='fade-right' viewBox="0 0 128 128">
+                    <div className='test-box' data-aos='fade-right'>
+                        <svg viewBox="0 0 128 128">
                     <path
                     fill="#E44D26"
                     d="M27.854 116.354l-8.043-90.211h88.378l-8.051 90.197-36.192 10.033z"
@@ -32,7 +33,11 @@ export default function Skills () {
                     d="M63.962 66.978v11.063h13.624L76.302 92.39l-12.34 3.331v11.51l22.682-6.286.166-1.87 2.6-29.127.27-2.97h-2.982zm0-22.395v11.064h26.725l.221-2.487.505-5.608.265-2.969z"
                     ></path>
                 </svg>
-                <svg data-aos='fade-right' viewBox="0 0 128 128">
+                <br/>
+                <span>HTML5</span>
+                </div>
+                <div className='test-box' data-aos='fade-right'>
+                <svg viewBox="0 0 128 128">
                     <path
                     fill="#fff"
                     d="M89.234 5.856H81.85l7.679 8.333v3.967H73.713v-4.645h7.678l-7.678-8.333V1.207h15.521v4.649zm-18.657 0h-7.384l7.679 8.333v3.967H55.055v-4.645h7.679l-7.679-8.333V1.207h15.522v4.649zm-18.474.19h-7.968v7.271h7.968v4.839H38.471V1.207h13.632v4.839z"
@@ -62,26 +67,61 @@ export default function Skills () {
                     d="M64.039 43.677v11.137H37.136l-.224-2.503-.507-5.646-.267-2.988h27.901zM64 66.221v11.138H51.753l-.223-2.503-.508-5.647-.267-2.988H64z"
                     ></path>
                 </svg>
-                <svg data-aos='fade-right' viewBox="0 0 128 128">
+                <br/>
+                <span>CSS3</span>
+                </div>
+                <div className='test-box' data-aos='fade-right'>
+                <svg viewBox="0 0 128 128">
                     <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
                     <path
                     fill="#323330"
                     d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zM69.462 58.943H57.753l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"
                     ></path>
                 </svg>
-                <img data-aos='fade-right'  alt="React-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/>
+                <br/>
+                <span>JavaScript</span>
+                </div>
+                <div className='test-box' data-aos='fade-right'>
+                <img  alt="React-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/>
+                <br/>
+                <span>React</span>
+                </div>
 
-                <img data-aos='fade-right' src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png' alt= 'typescript'/>
+                <div className='test-box' data-aos='fade-right'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png' alt= 'typescript'/>
+                <br/>
+                <span>TypeScript</span>
+                </div>
                 
-                <img data-aos='fade-right' src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt=""/>
-              
-               <img data-aos='fade-right' src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png' alt= 'python'/>
+                <div className='test-box' data-aos='fade-right'>
+                <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt=""/>
+                <br/>
+                <span>Redux</span>
+                </div>
+
+                <div className='test-box' data-aos='fade-right'>
+               <img src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png' alt= 'python'/>
+               <br/>
+               <span>Python</span>
+               </div>
+               
+               <div className='test-box' data-aos='fade-right'>
+                <img src='https://www.w3schools.in/wp-content/uploads/mongodb-logo.png?ezimgfmt=rs:0x0/rscb6/ng:webp/ngcb6' alt='mongodb'/>
+                <br/>
+               <span>MongoDB</span>
+               </div>
                 
-                <img data-aos='fade-right' src='https://www.w3schools.in/wp-content/uploads/mongodb-logo.png?ezimgfmt=rs:0x0/rscb6/ng:webp/ngcb6' alt='mongodb'/>
-                
-                <img data-aos='fade-right' src='https://cdn1.iconfinder.com/data/icons/soleicons-fill-vol-1/64/postgres_database_server_relational_dbms_sql-256.png' alt= 'postgres'/>
-                <img data-aos='fade-right' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png' alt= 'bootstrap'/>
-                
+               <div className='test-box' data-aos='fade-right'>
+                <img src='https://cdn1.iconfinder.com/data/icons/soleicons-fill-vol-1/64/postgres_database_server_relational_dbms_sql-256.png' alt= 'postgres'/>
+                <br/>
+               <span>Postgres</span>
+               </div>
+
+                <div className='test-box' data-aos='fade-right'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png' alt= 'bootstrap'/>
+                <br/>
+               <span>Bootstrap</span>
+               </div>
             </div>
         </div>
     )
